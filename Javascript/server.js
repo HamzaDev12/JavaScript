9d0b9f3b309e84487266f45236dbf75f3d346c60
Hamza Mustafa <hmustafe299@gmail.com>
2024-10-23 14:11:06 +0300

    arthimetic Operators

---
+++ Javascript/server.js
@@ -0,0 +1,47 @@
+// Variables 
+// let x
+// x= 100
+// console.log(x)
+
+// let age =23;
+// let proce = 10.00
+// let gpa = 2.1
+
+// console.log(`Your age is ${age}`)
+// console.log(`Your proce is ${proce}`)
+// console.log(`Your gpa is ${gpa}`)
+// console.log (typeof age)
+
+// let FirstName = "Hamza "
+// let lastName = "Dev"
+
+// console.log(FirstName + " " + lastName)
+// console.log (typeof FirstName)
+
+// let online = true
+
+// console.log(`Hamza Online ${online}`)
+
+// let fullname = "Hamza Mustafa"
+// let student = true
+// let jobtype = "web Developer"
+
+// document.getElementById("p1").textContent = `Your name is ${fullname}`;
+// document.getElementById("p2").textContent = `enrolled ${student}`;
+// document.getElementById("p3").textContent = `your jobtype is ${jobtype}`;
+
+// let students = 20
+// students = students + 2
+// students = students - 2
+// students = students * 2
+// students = students ** 2
+// students = students / 2
+// students = students % 2
+// students *= 4
+
+
+// console.log(students)
+
+let result = 2 + 3 / 4 * 4 + (34+1)
+
+console.log(result)
