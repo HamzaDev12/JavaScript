4cf91c1b7d9383b98050b8e18bd843db4ff423a2
Hamza Mustafa <hmustafe299@gmail.com>
2024-11-08 15:10:42 +0300

    Covered Funtions

--- Javascript/functions/main.js
+++ Javascript/functions/main.js
@@ -2,12 +2,15 @@ function myFunction (p1, p2) {
     return p1 * p2;
 }
 
+
 let result = myFunction(4, 3)
 document.getElementById("demo").innerHTML = result
+
 
 function greet(name) {
     console.log ("Hello " + name)
 }
+greet("Hamza")
 
 const Salan = (name) => {
     console.log ("Hello " + name)
@@ -18,6 +21,7 @@ function add(a , b){
 }
 console.log (add(3, 4))
 
+add()
 function multply(c , d) {
     return c * d
 }
@@ -28,3 +32,26 @@ document.getElementById("demo1").innerHTML = natijo
 setTimeout(function() { 
     console.log("This runs after 2 seconds"); 
     }, 2000);
+
+    (function() {
+        console.log("This runs after 3 seconds")
+    }) ()
+
+function OperationApply (a, b, Apply) {
+    return Apply(a, b)
+} console.log(OperationApply(10, 5, add))
+
+function Calling(callback){
+    setTimeout(() => {
+        callback("Hamza: Calling")
+    }, 1000);
+}
+
+Calling((message)=>{
+    console.log(message)
+})
+
+function fectorial(n){
+    if (n <= 1) return 1
+    return n * fectorial(n - 1)
+}console.log(fectorial(4))
